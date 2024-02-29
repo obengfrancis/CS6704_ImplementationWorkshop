f3f8f325b88f902ed121485af7ab8b0497feae20
minhyukko <57718746+minhyukko@users.noreply.github.com>
2024-02-29 13:24:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+# connecting to the database
+connection = sqlite3.connect("gfg.db")
+
+# cursor
+crsr = connection.cursor()
+
+crsr.execute(sql_drop_user_table)
+crsr.execute(sql_drop_post_table)
+crsr.execute(sql_drop_community_table)
+
+# SQL command to create a table in the database
+sql_command = """CREATE TABLE Users ( 
+userId INTEGER PRIMARY KEY, 
+name VARCHAR(20), 
+bio VARCHAR(30), 
+researchField VARCHAR(30), 
+posts INTEGER);
+"""
+
+sql_command_2 = """CREATE TABLE Post ( 
+postId INTEGER PRIMARY KEY, 
+title VARCHAR(20), 
+datetime INTEGER);
+"""
+
+sql_command_3 = """CREATE TABLE Communities (
+users VARCHAR(20),
+posts VARCHAR(20));
+"""
+
+sql_drop_user_table = "DROP TABLE IF EXISTS Users;"
+sql_drop_post_table = "DROP TABLE IF EXISTS Post;"
+sql_drop_community_table = "DROP TABLE IF EXISTS Communities;"
+
+
+# execute the statement
+crsr.execute(sql_command)
+crsr.execute(sql_command_2)
+crsr.execute(sql_command_3)
+
+sql_drop_user_table = "DROP TABLE IF EXISTS Users;"
+sql_drop_post_table = "DROP TABLE IF EXISTS Post;"
+sql_drop_community_table = "DROP TABLE IF EXISTS Communities;"
+
+# close the connection
+connection.close()
